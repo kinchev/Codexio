7180659456299b6c28b1050f47e06347943d3eb8
kinchev <kinchev@yahoo.com>
2022-11-09 16:54:31 +0200

    Tasks1.java

---
+++ Tasks/src/Tasks1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Tasks1 {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Map<String,Integer> box = new HashMap<>();
+
+        while(!input.equals("End")){
+
+            List<String> colorsAndCount = new ArrayList<>(Arrays.asList(input.split(":")));
+            String color = colorsAndCount.get(0);
+            int count = Integer.parseInt(colorsAndCount.get(1));
+            colorsAndCount.clear();
+            box.merge(color,count,Integer::sum);
+
+
+
+
+            input= scanner.nextLine();
+        }
+
+        int sum=0;
+        int max=Collections.max(box.values());
+        for (int x: box.values()) {
+            sum += x;
+        }
+
+
+        System.out.println(sum-=max);
+    }
+}
